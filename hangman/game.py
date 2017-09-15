7fae63f848b5ab7ba3e545751ccf15adfec67dde
Phuc Tran <chubby018@gmail.com>
2017-09-15 08:19:19 +0000

    completed assignments

--- hangman/game.py
+++ hangman/game.py
@@ -1,23 +1,67 @@
 from .exceptions import *
+import random
 
 # Complete with your own, just for fun :)
 LIST_OF_WORDS = []
 
 
 def _get_random_word(list_of_words):
-    pass
-
+    if not list_of_words:
+        raise InvalidListOfWordsException
+    else:
+        return random.choice(list_of_words)
 
 def _mask_word(word):
-    pass
+    if not word:
+        raise InvalidWordException
+    else:
+        return '*' * len(word)
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    if not answer_word or not masked_word:
+        raise InvalidWordException()
+    if len(answer_word) != len(masked_word):
+        raise InvalidWordException()
+    if len(character) > 1:
+        raise InvalidGuessedLetterException()
+        
+    word = ''
+        
+    if character.lower() not in answer_word.lower():
+        return masked_word
+
+    for index in range(len(answer_word)):
+        if character.lower() == answer_word[index].lower():
+            word += answer_word[index].lower()
+        else:
+            word += masked_word[index]
+
+    return word
 
 
 def guess_letter(game, letter):
-    pass
+    if game['answer_word'].lower() == game['masked_word'].lower() or game['remaining_misses'] < 1:
+        raise GameFinishedException()
+    if letter.lower() in game['previous_guesses']:
+        raise InvalidGuessedLetterException()
+
+    masked = game['masked_word']
+    word = _uncover_word(game['answer_word'], masked, letter)
+
+    if masked == word:
+        game['remaining_misses'] -= 1
+    else:
+        game['masked_word'] = word
+
+    game['previous_guesses'].append(letter.lower())
+    
+    if game['answer_word'].lower() == game['masked_word'].lower():
+        raise GameWonException()
+    if game['remaining_misses'] < 1:
+        raise GameLostException()
+
+    return word
 
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
